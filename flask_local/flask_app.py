f91fc21234f399d51f67449e001356ac4e0551f3
payerhans <32729314+payerhans@users.noreply.github.com>
2019-06-26 09:19:17 +0200

    gestern kann in flask dargestellt werden

--- flask_local/flask_app.py
+++ flask_local/flask_app.py
@@ -1,27 +1,33 @@
 from flask import Flask, render_template
 from graph import build_graph
+import sqlite3
+import pandas as pd
+from datetime import datetime, date, time, timedelta
  
 app = Flask(__name__)
  
 @app.route('/graphs')
 def graphs():
-    #These coordinates could be stored in DB
-    x1 = [0, 1, 2, 3, 4]
-    y1 = [10, 30, 40, 5, 50]
-    x2 = [0, 1, 2, 3, 4]
-    y2 = [50, 30, 20, 10, 50]
-    x3 = [0, 1, 2, 3, 4]
-    y3 = [0, 30, 10, 5, 30]
- 
-    graph1_url = build_graph(x1,y1);
-    graph2_url = build_graph(x2,y2);
-    graph3_url = build_graph(x3,y3);
- 
-    return render_template('graphs.html',
-    graph1=graph1_url,
-    graph2=graph2_url,
-    graph3=graph3_url)
- 
+    now = date.today()
+    now_str = now.strftime("%Y-%m-%d")
+    yesterday = now-timedelta(days=1)
+    yesterday_str = yesterday.strftime("%Y-%m-%d")
+    # connect to sqlite database
+    conn = sqlite3.connect('../flask_local/temp_bude.db')
+    query = "SELECT temp, sqltime FROM sensor_data where sqltime between '{}' and '{}'".format(yesterday_str, now_str)
+    # insert into dataframe
+    df = pd.read_sql_query(query,conn)
+    #define colums
+    df.columns = ['temp', 'sqltime']
+    #set column sqltime to panda datetimeformat
+    df['sqltime'] = pd.to_datetime(df['sqltime'], format="%Y-%m-%d %H:%M:%S")
+    # create indexed panda
+    indexed_df = df.set_index('sqltime')
+    ts = indexed_df['temp']
+    graph1_url = build_graph(ts)
+    return render_template('graphs.html', graph1=graph1_url, yesterday_str=yesterday_str)
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
--- flask_local/graph.py
+++ flask_local/graph.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import io
 import base64
  
-def build_graph(x_coordinates, y_coordinates):
+def build_graph(ts):
     img = io.BytesIO()
-    plt.plot(x_coordinates, y_coordinates)
+    plt.plot(ts)
     plt.savefig(img, format='png')
     img.seek(0)
     graph_url = base64.b64encode(img.getvalue()).decode()
